a9690af3e97be7ab916b00a2b0aa0310caea16b6
EringiShimeji <eringi.shimeji.5296@gmail.com>
2022-02-18 09:33:55 +0900

    station10

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -1,1 +1,32 @@
-// DO NOT DELETE
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/**
+ *
+ * @param {{ breeds: string[], selectedBreed: string, onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void }} props
+ * @returns
+ */
+const BreedsSelect = ({ breeds, selectedBreed, onChange }) => {
+  return (
+    <select
+      name="breed"
+      id="breed-select"
+      value={selectedBreed}
+      onChange={onChange}
+    >
+      {breeds.map(breed => (
+        <option key={breed} value={breed}>
+          {breed}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+BreedsSelect.propTypes = {
+  breeds: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selectedBreed: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+}
+
+export default BreedsSelect
