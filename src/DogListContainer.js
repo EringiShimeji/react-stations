e20de3e9717d47757a602f18400fbf190e9a0c67
EringiShimeji <eringi.shimeji.5296@gmail.com>
2022-02-18 09:59:32 +0900

    station11

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -4,6 +4,7 @@ import BreedsSelect from './BreedsSelect'
 const DogListContainer = () => {
   const [breeds, setBreeds] = useState([])
   const [selectedBreed, setSelectedBreed] = useState()
+  const [certainBreedUrls, setCertainBreedUrls] = useState([])
 
   /**
    *
@@ -13,26 +14,50 @@ const DogListContainer = () => {
     setSelectedBreed(e.target.value)
   }
 
+  const handleClick = async () => {
+    if (!selectedBreed) return
+
+    const apiResponse = await fetch(
+      `https://dog.ceo/api/breed/${selectedBreed}/images/random/3`,
+    ).then(r => r.json())
+
+    if (apiResponse.status !== 'success') return
+
+    const newCertainBreedUrls = apiResponse.message
+
+    setCertainBreedUrls(newCertainBreedUrls)
+  }
+
   useEffect(() => {
     const fetchBreeds = async () => {
       const apiResponse = await fetch(
         'https://dog.ceo/api/breeds/list/all',
       ).then(r => r.json())
+
+      if (apiResponse.status !== 'success') return
+
       const newBreeds = Object.keys(apiResponse.message)
 
       setBreeds(newBreeds)
+      setSelectedBreed(newBreeds[0])
     }
 
     fetchBreeds()
   }, [])
 
   return (
-    <div>
+    <div className="dog-list-container">
       <BreedsSelect
         breeds={breeds}
         selectedBreed={selectedBreed}
         onChange={handleBreedsSelectChange}
       />
+      <button onClick={handleClick}>表示</button>
+      <div className="dog-list-container__breed-img-container">
+        {certainBreedUrls.map(url => (
+          <img key={url} src={url} alt={`${selectedBreed} image`} />
+        ))}
+      </div>
     </div>
   )
 }
